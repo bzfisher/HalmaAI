57bb9d9266745a9bf9aa9f9d876ba4940908fe65
ben f <ben.fisher@mail.mcgill.ca>
2014-03-30 12:26:00 -0400

    Initial commit

---
+++ src/s260488774/s260488774Player.java
@@ -0,0 +1,25 @@
+package s260488774;
+
+import halma.CCBoard;
+import boardgame.Board;
+import boardgame.Move;
+import boardgame.Player;
+
+public class s260488774Player extends Player
+{
+
+    /** Provide a default public constructor */
+    public s260488774Player() { super("random"); }
+    public s260488774Player(String s) { super(s); }
+    
+    public Board createBoard() { return new CCBoard(); }
+
+    
+	@Override
+	public Move chooseMove(Board board)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
