3e6905a73a8e3fe06010fa4a750f23316b1d6274
ben f <ben.fisher@mail.mcgill.ca>
2014-04-05 16:04:04 -0400

    added 3 more heuristics.

--- src/s260488774/HalmaHeuristics.java
+++ src/s260488774/HalmaHeuristics.java
@@ -14,20 +14,52 @@ public class HalmaHeuristics
 	/**
 	 * the "farthest" base point for each player.
 	 */
-	private static Point[] bases = {	new Point(0,0), 
+	private static Point[] cornerBasePoint= {	new Point(0,0), 
 		new Point(15,0),
 		new Point(0,15),
 		new Point(15,15)};
+
+	private static Point[][] edgeBasePoints = 
+		{
+		{
+			new Point(0,0),new Point(3,0),new Point(2,0),new Point(1,0),new Point(0,3),new Point(0,2),new Point(0,1)
+		},
+		{
+			new Point(15,0),new Point(14,0),new Point(13,0),new Point(12,0),new Point(15,3),new Point(15,2),new Point(15,1)
+		},
+		{
+			new Point(0,15),new Point(3,15),new Point(2,15),new Point(1,15),new Point(0,12),new Point(0,14),new Point(0,13)
+		},
+		{
+			new Point(13,15),new Point(12,15),new Point(15,15),new Point(14,15),new Point(15,12),new Point(15,14),new Point(15,13)
+		}
+		};
 	
+	private static Point[][] nonEdgeBasePoints = 
+		{
+		{
+			new Point(2,2),new Point(1,2),new Point(2,1),new Point(1,1),new Point(3,1),new Point(1,3)
+		},
+		{
+			new Point(14,3),new Point(14,1),new Point(14,2),new Point(12,1),new Point(13,2),new Point(13,1)
+		},
+		{
+			new Point(2,13),new Point(1,13),new Point(3,14),new Point(1,12),new Point(2,14),new Point(1,14)
+		},
+		{
+			new Point(12,14),new Point(14,12),new Point(13,14),new Point(14,14),new Point(14,13),new Point(13,13)
+		}
+		};
+
 	/**
 	 * the furthest target zone point for each player.
 	 */
-	private static Point[] targets = {	new Point(15,15), 
+	private static Point[] targetCorners = {	new Point(15,15), 
 		new Point(0,15),
 		new Point(15,0),
 		new Point(0,0)};
 
-	
+	private static int[] oppositePlayerID = {3, 2, 1, 0};
 	/**
 	 * @param board the current gameboard.
 	 * @param currPlayer the player for which we wish to calculate the board score.
@@ -65,7 +97,7 @@ public class HalmaHeuristics
 		double playerDist = 0;
 		for (Point pt: board.getPieces(playerID))
 		{
-			playerDist = playerDist+manhattanDistance(pt,bases[playerID]);
+			playerDist = playerDist+manhattanDistance(pt,cornerBasePoint[playerID]);
 		}
 		return playerDist;
 	}
@@ -129,19 +161,89 @@ public class HalmaHeuristics
 	 */
 	private static double splitDistance(int playerID, CCBoard board)
 	{
-		Point furthestPt = bases[playerID];
-		Point closestPt = targets[playerID];
+		Point furthestPt = cornerBasePoint[playerID];
+		Point closestPt = targetCorners[playerID];
 		for (Point pt: board.getPieces(playerID))
 		{
-			if (manhattanDistance(closestPt, bases[playerID])>manhattanDistance(pt, bases[playerID]))
+			if (manhattanDistance(closestPt, cornerBasePoint[playerID])>manhattanDistance(pt, cornerBasePoint[playerID]))
 			{
 				closestPt = pt;
 			}
-			if (manhattanDistance(furthestPt, bases[playerID])<manhattanDistance(pt, bases[playerID]))
+			if (manhattanDistance(furthestPt, cornerBasePoint[playerID])<manhattanDistance(pt, cornerBasePoint[playerID]))
 			{
 				furthestPt = pt;
 			} 
 		}
 		return manhattanDistance(furthestPt, closestPt);
 	}
+
+	
+	/**
+	 * Checks if the player won. Higher is better.
+	 * @param ID the player ID we are checking for. 
+	 * @param board the board game as it currently is.
+	 * @return 1 if the player has won, 0 if not.
+	 */
+	private double checkIfWin(int ID, CCBoard board){
+		assert(ID<4);
+		boolean win=true;
+		int base_id= ID^3;
+		Integer IDInteger= new Integer(ID);
+
+		for(Point p: edgeBasePoints[base_id]){
+			win &= IDInteger.equals(board.getPieceAt(p));
+		}
+		for(Point p: nonEdgeBasePoints[base_id]){
+			win &= IDInteger.equals(board.getPieceAt(p));
+		}
+		if (win) return 1;
+		return 0;
+	}
+	
+	/**
+	 * checks how many pieces are at the edges of the target zone. Higher is better. 
+	 * @param ID the player ID we are checking for.
+	 * @param board the current gameboard.
+	 * @return number of pieces at edge of target zone.
+	 */
+	private double NumberOfPiecesAtEdgeOfTarget(int ID, CCBoard board)
+	{
+		double edgeResult = 0;
+		for (Point piece: board.getPieces(ID))
+		{
+			for (Point targetPoint: edgeBasePoints[oppositePlayerID[ID]])
+			{
+				if (manhattanDistance(piece, targetPoint)==0)
+				{
+					edgeResult++;
+				}
+			}
+		}
+		return edgeResult;
+	}
+	
+	/**
+	 * 	 * checks how many pieces are at the  non-edges of the target zone. Higher is better. 
+	 * @param ID the ID to check for. 
+	 * @param board the current game board.
+	 * @return the number of pieces at the non-edge of the target zone.
+	 */
+	private double NumberOfPiecesAtNonEdgeOfTarget(int ID, CCBoard board)
+	{
+		double nonEdgeResult = 0;
+		for (Point piece: board.getPieces(ID))
+		{
+			for (Point targetPoint: nonEdgeBasePoints[oppositePlayerID[ID]])
+			{
+				if (manhattanDistance(piece, targetPoint)==0)
+				{
+					nonEdgeResult++;
+				}
+			}
+		}
+		return nonEdgeResult;
+	}
+	
+	
+
 }
