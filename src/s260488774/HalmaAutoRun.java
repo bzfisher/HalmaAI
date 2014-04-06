133c513bb7e1a6955f082f20e4a69fba5bc95958
ben f <ben.fisher@mail.mcgill.ca>
2014-04-06 12:05:28 -0400

    Added console logging to acknowledge running process.

--- src/s260488774/HalmaAutoRun.java
+++ src/s260488774/HalmaAutoRun.java
@@ -1,7 +1,4 @@
 package s260488774;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
@@ -12,32 +9,33 @@ public class HalmaAutoRun
 	static Random r = new Random();
 
 	public static void main( String[] args ) throws IOException {
-
-		for(int i = 0; i<3; i++)
+		System.out.println("running!");
+		ArrayList<String> runCommands = new ArrayList<String>();
+		runCommands.add("java -cp /root/projectsrc.jar:/root/src boardgame.Client s260488774.s260488774AlphaBetaPlayer");
+		runCommands.add("java -cp /root/projectsrc.jar:/root/src boardgame.Client s260488774.s260488774minimaxPlayer");
+		runCommands.add("java -cp /root/projectsrc.jar:/root/src boardgame.Client s260488774.s260488774PlayerHeuristic");
+		Process p;
+		while(true)
 		{
-			ArrayList<String> runCommands = new ArrayList<String>();
-			runCommands.add("java -cp ~/projectsrc.jar:/Users/Ben/git/HalmaAI/src boardgame.Client s260488774.player1");
-			runCommands.add("java -cp ~/projectsrc.jar:/Users/Ben/git/HalmaAI/src boardgame.Client s260488774.player1");
-			runCommands.add("java -cp ~/projectsrc.jar:/Users/Ben/git/HalmaAI/src boardgame.Client s260488774.player1");
-			runCommands.add("java -cp ~/projectsrc.jar:/Users/Ben/git/HalmaAI/src boardgame.Client s260488774.player1");
 			StringBuffer combinedCommands = new StringBuffer();
-			while(!runCommands.isEmpty())
+			for (int j=0; j<4; j++)
 			{
 				int nextCommandIndex = r.nextInt(runCommands.size());
 				combinedCommands.append(runCommands.get(nextCommandIndex));
-				runCommands.remove(nextCommandIndex);
 				combinedCommands.append(" & ");
 			}
+			
 			String[] cmd = {
 					"/bin/sh",
 					"-c",
 					combinedCommands.toString()
 			};
-			Runtime.getRuntime().exec(cmd);
+			p = Runtime.getRuntime().exec(cmd);
 
 			try
 			{
-				Thread.sleep(30000);
+				Thread.sleep(90000);
+				p.destroy();
 			}
 			catch (InterruptedException e)
 			{
@@ -46,53 +44,6 @@ public class HalmaAutoRun
 			}
 
 		}
-	}
-
-	public void recalculateWeights(String playerFile, String outcomeFile) throws IOException
-	{
-		ArrayList<Double> previousWeights = new ArrayList<Double>();
-		BufferedReader br = new BufferedReader(new FileReader(playerFile));
-		try {
-			String line = br.readLine();
-			while (line!=null)
-			{
-				previousWeights.add(Double.parseDouble(line));
-				line = br.readLine();
-			}
-		}
-		finally {
-			br.close();
-		}
-		BufferedReader outcomeFileReader = new BufferedReader(new FileReader(outcomeFile));
-		String lastLine = null;
-		try {
-			String line = outcomeFileReader.readLine();
-			while (line!=null)
-			{
-				lastLine = line;
-				line = br.readLine();
-			}
-		}
-		finally {
-			br.close();
-		}
-		boolean team0Winner = false;
-		boolean team1Winner = false;
-		if (lastLine.split("Player-0").length>1) team0Winner = true;
-		else if (lastLine.split("Player-1").length>1) team1Winner = true;
-		
-	}
-	
-//	public boolean isWinner(int playerID)
-//	{
-//		
-//	}
-
-	private double updateWeight(double previousWeight, int n, int v, double k)
-	{
-		double top = (previousWeight*(n-1))  + (k*v);
-		double bottom = (n+k-1);
-		return top/bottom;
 	}
 
 }
